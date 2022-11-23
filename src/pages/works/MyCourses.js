779bcb867f52464294534306795806edf07d65e9
Mor-ald <72343402+Mor-ald@users.noreply.github.com>
2022-11-23 10:59:03 +0300

    PORTFOLIO-6 add files

---
+++ src/pages/works/MyCourses.js
@@ -0,0 +1,13 @@
+export const myCourses = [
+    {name: "Программирование на Python, Stepik", link: "https://stepik.org/course/67/syllabus"},
+    {name: "Веб-программирование, OpenEdu", link: "https://openedu.ru/course/ITMOUniversity/WEBDEV/"},
+    {name: "Первичная обработка и хранение данных, OpenEdu", link: "https://openedu.ru/course/ITMOUniversity/DATAN/"},
+    {name: "Введение в машинное обучение, OpenEdu", link: "https://openedu.ru/course/ITMOUniversity/INTROML/?session=fall_2021_ITMO"},
+    {name: "JavaScript/​DOM/​Интерфейсы", link: "https://learn.javascript.ru/courses/js"},
+    {name: "Продвинутое машинное обучение, OpenEdu", link: "https://openedu.ru/course/ITMOUniversity/ADVML/?session=spring_2022_ITMO"},
+    {name: "Серверные веб-технологии и системы управления контентом", link: "https://openedu.ru/course/ITMOUniversity/NODEJS/?session=fall_2022"},
+    {name: "Data Manipulation with pandas, Datacamp", link: "https://www.datacamp.com/courses/data-manipulation-with-pandas"},
+    {name: "Building Recommendation Engines in Python, Datacapm", link: "https://www.datacamp.com/courses/building-recommendation-engines-in-python"},
+    {name: "Cluster Analysis in Python, Datacamp", link: "https://www.datacamp.com/courses/cluster-analysis-in-python"},
+    {name: "Machine Learning, Datacamp", link: "https://www.datacamp.com/courses/understanding-machine-learning"},
+];
---
+++ src/pages/works/MyWorks.js
@@ -0,0 +1,13 @@
+const works = [
+    {name: "Анализ времени сборки проекта на JAMstack генераторами статических сайтов в зависимости от числа страниц", link: "https://disk.yandex.ru/i/Hpxh41mpTKPk4g"},
+    {name: "Анализ зарубежных и отечественных научных источников по теме исследования «Исследование современных подходов к проектированию и разработке на JAMstack»", link: "https://disk.yandex.ru/i/16VajTJ1NMVMCA"},
+    {name: "Проектирование схемы базы данных для интернет-магазина по продаже автотоваров и автозапчастей", link: "https://disk.yandex.ru/i/Nl3MkkIYOLFntQ"},
+    {name: "Современные методы и инструменты проектирования и разработки веб-приложений и приложений на архитектуре JAMstack", link: "https://disk.yandex.ru/i/rTH0GqQ4Fnu8xg"},
+    {name: "Создание структуры базы данных", link: "https://disk.yandex.ru/i/XlR9tvyJh9oSgg"},
+    {name: "Улучшение структуры базы данных", link: "https://disk.yandex.ru/i/z50OdeB-hie-RA"},
+    {name: "Топ 10 команд докера", link: "https://mor-ald.github.io/Docker-4/"},
+    {name: "Чекинаторы веб (2 семестр)", link: "https://github.com/Mor-ald/xsl"},
+    {name: "Задание по докеру (счетчик c БД)", link: "https://github.com/Mor-ald/task7_docker"},
+    {name: "Задание на асинхронность", link: "https://replit.com/@Morald/goss3?v=1"},
+    {name: "Веб-приложение на node.js по заголовкам", link: "https://replit.com/@Morald/goss4?v=1"},
+]
